4ed94d65982da6bddd70782d7e8dc1b28348a96b
Emil F. Mirzayev <emilfmi@code.local>
2025-11-20 17:59:49 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+//tapsirig 1
+// num = 20;
+// if (num % 3 == 0 && num % 7 == 0) {
+//     console.log("bu eded 3-e ve 7 - e  bolunur");
+// }   
+// else{
+//     console.log("bu eded 3-e ve 7-e  bolunmur");
+// }
+
+//tapsirig 2 
+
+// n = 5;
+// m = 10;
+// count = 0;
+// for (let i = n; i <= m; i++) {
+//     if (i % 2 != 0) {
+//         count ++;
+//     }
+// }
+// console.log(count);
+
+//tapsirig 3
+// n = 5;
+// m = 10;
+// sum = 0;
+// for (let i = n; i <= m; i++) {
+//     if (i % 2 == 1) {
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+//tapsirig 4
+
+//tapsirig 5
+// n = 8;
+// isPrimary = true;
+// for(let i = 2; i < n**(1/2); i++){
+//     if (n % i == 0){
+//         console.log("murekkeb ededdir");
+//         isPrimary = false;
+//         break;
+//     }
+// }
+// if (isPrimary){
+//     console.log("sade ededdir");
+// }
